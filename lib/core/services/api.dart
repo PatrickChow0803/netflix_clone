160525c666ff62876d852ae8fb2194930d5b33d8
Patrick Chow <patrickchow0803@gmail.com>
2021-06-11 20:19:45 -0500

    adjusted the url to be used for making api calls

--- lib/core/services/api.dart
+++ lib/core/services/api.dart
@@ -1,11 +1,13 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 
 class Api {
-  static const uri = 'https://api.themoviedb.org/3/';
+  static final APIKEY = dotenv.env['APIKEY'];
+  static const uri = 'https://api.themoviedb.org/3/movie/';
 
   static Future<Response> get(String path) async {
     try {
-      final request = Request('GET', Uri.parse('$uri/$path'));
+      final request = Request('GET', Uri.parse('$uri/$path?api_key=$APIKEY'));
 
       final Response response = await Response.fromStream(
         await request.send().timeout(Duration(seconds: 30)),
