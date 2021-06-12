b55a63c16cfcc36fc773201ef13b8a81053d27f3
Patrick Chow <patrickchow0803@gmail.com>
2021-06-11 19:32:40 -0500

    Added Api class

---
+++ lib/core/services/api.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart';
+
+class Api {
+  static const uri = '';
+
+  static Future<Response> get(String path) async {
+    try {
+      final request = Request('GET', Uri.parse('$uri/$path'));
+
+      final Response response = await Response.fromStream(
+        await request.send().timeout(Duration(seconds: 30)),
+      );
+
+      return response;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
