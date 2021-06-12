047d32a57ac7c92bc79ac6ce11a41e16f4eda58a
Patrick Chow <patrickchow0803@gmail.com>
2021-06-11 20:10:23 -0500

    added dot_env to use apikey

--- lib/core/services/api.dart
+++ lib/core/services/api.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart';
 
 class Api {
-  static const uri = '';
+  static const uri = 'https://api.themoviedb.org/3/';
 
   static Future<Response> get(String path) async {
     try {
